a4599c11c120b8c80323113a1e7df71862863162
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2022-08-22 18:25:26 -0300

    adicionado menu inferior

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch,BrowserRouter } from 'react-router-dom';
+import Drinks from './Pages/Drinks';
+import Foods from './Pages/Foods';
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
+import DoneRecepies from './Pages/DoneRecepies';
+import FavoriteRecepes from './Pages/FavoriteRecipes';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Foods } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecepies } />
+        <Route path="/favorite-recipes" component={ FavoriteRecepes } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/DoneRecepies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function DoneRecepies() {
+  return (
+    <div>
+      <h1>DoneRecepies</h1>
+
+    </div>
+
+  );
+}
+export default DoneRecepies;
