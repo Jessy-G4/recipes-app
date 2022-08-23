5f37f6fc6f68ea3fa150a81110bff4c34c4b7f68
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2022-08-23 15:03:24 -0300

    alteração no menu inferior

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import RootProvider from './context/RootProvider';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <RootProvider>
