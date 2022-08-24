ad0a42f671cf09f5c755d0abf7b0b6bc48c3fc7e
Jessy Cristina <jessyaraujo395@gmail.com>
2022-08-24 16:35:55 -0300

    pages minusculo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 // Importações
-import Login from './Pages/Login';
+import Login from './pages/Login';
 
 function App() {
   return (
